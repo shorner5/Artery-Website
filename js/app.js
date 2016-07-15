26fec67edeff9a95d3f867e2f9b0180142e9cc83
shorner5 <shorner5@live.com>
2016-07-14 20:32:03 -0700

    added spring and made resizing work

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
-var names = ["Adrian, 29",  "Olivia, 26", "Stuart, 20","Jamal, 28","Oliver, 24" , "Jaime, 30", "Pedro, 19", "Joseph, 31", "Johnny, 26", "Fred, 21"];
+var names = ["Adrian, 29",  "Olivia, 20", "Stuart, 20","Jamal, 28","Oliver, 24" , "Jaime, 30", "Pedro, 19", "Joseph, 31", "Ava, 26", "Fred, 21"];
 var images = ["img/j.jpg", "img/i.png", "img/h.jpg", "img/g.jpg", "img/f.png", "img/e.jpg", "img/d.jpg", "img/c.jpg", "img/b.jpg", "img/a.jpg"];
+var position;
+var pos_set = false;
 
 var main = function() {
 	populateCards();
 }
 
 function setDraggable(elem) {
-	position = elem.position();
-	elem.draggable({
+	elem.draggable({ 
+		scroll: false,
+    	start: function(event, ui) {
+        	ui.position.left = 0;
+        	ui.position.top = 0;
+    	},
   		stop: function(event, ui) {
-  			onSwipe(elem.position().left - position.left, elem);
+  			onSwipe(elem.position().left - position.left, elem.position().top - position.top, elem);
   		},
   		drag: function(event, ui) {
-  			distance = -(position.left - ui.position.left)/10;
-  			elem.animate({rotate: distance+'deg'}, 1);
+  			if (pos_set == false) {
+  				position = elem.position();
+  				console.log(position);
+  				pos_set = true;
+  			}
+  			else {
+	  			distance = -(position.left - ui.position.left)/10;
+				if (distance < 30 && distance > -20) {
+					elem.animate({rotate: distance+'deg'}, 0);
+				}
+			}
   		}
-
 	});
 }
 
@@ -29,18 +43,20 @@ function populateCards() {
 
 function decorateElem(elem, i) {
 	elem.zIndex(i + 1);
-	var name = document.querySelector("#container").querySelector("#card").querySelector("#card-info");
-	var img_src = document.querySelector("#container").querySelector("#card").querySelector("#card-image");
+	var name = document.querySelector("#card").querySelector("#card-info");
+	var img_src = document.querySelector("#card");
 	$(name).html(names[i]);
-	img_src.src = images[i];
+	img_src.style.backgroundImage="url(" + images[i] + ")";
 	setDraggable(elem);
 }
 
-function onSwipe(distance, elem) {
+function onSwipe(distance, distTop, elem) {
 	if (distance > 200) {
 				 elem.animate({
                 left: '150%',
-            }, 400 );
+            }, 400, function() {
+            	elem.hide();
+            } );
 		elem.draggable( "destroy" );
 	}
 	else if (distance < -300) {
@@ -50,13 +66,28 @@ function onSwipe(distance, elem) {
 		 elem.draggable( "destroy" );
 	}
 	else {
-		elem.animate({left: position.left,
-							top: position.top,
-							rotate: '0deg'}, 
-							300, function() {
-		    // Animation complete.
-		  });
+		console.log(distance / 4 + ", " + distTop /4);
+		if (Math.abs(distance / 4) > 10 || Math.abs(distTop / 4) > 10) {
+			elem.animate({left: position.left - (distance / 4),
+								top: position.top - (distTop / 4),
+								rotate: '0deg'}, 
+								200, function() {
+			    elem.animate({left: position.left,
+								top: position.top}, 
+								100);
+			});
+		}
+		else {
+			 elem.animate({left: position.left,
+								top: position.top,
+								rotate: '0deg'}, 
+								200);
+		}
 	}
 }
 
 $(document).ready(main);
+
+function resize() {
+	pos_set = false;
+}
